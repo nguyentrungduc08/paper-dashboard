4e9d0453bc2761727b7e4b6681974824dea8f33b
nguyentrungduc <nguyentrungduc08@gmail.com>
2019-11-05 23:33:47 +0700

    add resources pro

---
+++ page_pro/crawl.sh
@@ -0,0 +1,23 @@
+curl -O https://demos.creative-tim.com/paper-dashboard-2-pro/examples/pages/login.html
+
+curl -O https://demos.creative-tim.com/paper-dashboard-2-pro/examples/pages/register.html
+
+curl -O https://demos.creative-tim.com/paper-dashboard-2-pro/examples/pages/lock.html
+
+curl -O https://demos.creative-tim.com/paper-dashboard-2-pro/examples/pages/user.html
+
+curl -O https://demos.creative-tim.com/paper-dashboard-2-pro/examples/components/buttons.html
+
+curl -O https://demos.creative-tim.com/paper-dashboard-2-pro/examples/components/grid.html
+curl -O https://demos.creative-tim.com/paper-dashboard-2-pro/examples/components/panels.html
+curl -O https://demos.creative-tim.com/paper-dashboard-2-pro/examples/components/sweet-alert.html
+curl -O https://demos.creative-tim.com/paper-dashboard-2-pro/examples/components/notifications.html
+curl -O https://demos.creative-tim.com/paper-dashboard-2-pro/examples/components/icons.html
+curl -O https://demos.creative-tim.com/paper-dashboard-2-pro/examples/components/typography.html
+curl -O https://demos.creative-tim.com/paper-dashboard-2-pro/examples/forms/regular.html
+curl -O https://demos.creative-tim.com/paper-dashboard-2-pro/examples/forms/extended.html
+
+curl -O https://demos.creative-tim.com/paper-dashboard-2-pro/examples/forms/validation.html
+curl -O https://demos.creative-tim.com/paper-dashboard-2-pro/examples/forms/wizard.html
+curl -O https://demos.creative-tim.com/paper-dashboard-2-pro/examples/tables/datatables.net.html
+curl -O https://demos.creative-tim.com/paper-dashboard-2-pro/examples/charts.html
